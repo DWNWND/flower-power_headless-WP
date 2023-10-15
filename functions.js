bd2cbfa1a90b8295e2d353e2f209fe5dc483dcc6
Thea <theola51924@stud.noroff.no>
2023-10-15 13:39:52 +0200

    testing out if the headless plugin works

--- functions.js
+++ functions.js
@@ -1,10 +1,14 @@
 const baseUrl = "http://ft-23-online-cms-flower-power.local/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products");
 
 async function getProducts(url) {
   try {
     const response = await fetch(url);
     const products = await response.json();
-    console.log(products);
+    productContainer.innerHTML += `
+    <div class="product"><h2>${product.name}</h2>
+    <div class="product-image" style="background-image:url('${product.image})</div>
+    </div>`;
   } catch (error) {
     console.log(error);
   }
