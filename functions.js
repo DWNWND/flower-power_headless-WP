768eeebb1a773255e32c17b561cf765a35217d22
Thea <theola51924@stud.noroff.no>
2023-10-15 15:31:01 +0200

    fetched api from WP and displayed on this site. also redirected the WP site to this site

--- functions.js
+++ functions.js
@@ -5,10 +5,13 @@ async function getProducts(url) {
   try {
     const response = await fetch(url);
     const products = await response.json();
-    productContainer.innerHTML += `
-    <div class="product"><h2>${product.name}</h2>
-    <div class="product-image" style="background-image:url('${product.image})</div>
-    </div>`;
+    console.log(products);
+    products.forEach(function (product) {
+      productContainer.innerHTML += `
+      <div class="product"><h2>${product.name}</h2>
+      <div class="product-image" style="background-image:url('${product.images[0].src}')"></div>
+      <p class="price">Price $ ${product.prices.price}<p></div>`;
+    });
   } catch (error) {
     console.log(error);
   }
